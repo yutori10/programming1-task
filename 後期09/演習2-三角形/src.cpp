60977df6cc7522c67c5e9ba666ca18dafdfcbbb4
yutori10 <yutori1107@gmail.com>
2022-12-22 15:40:58 +0900

    triangle

--- 後期09/演習2-三角形/src.cpp
+++ 後期09/演習2-三角形/src.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main() {
     int len_A, len_B, Deg_C;
+
+    cin >> len_A >> len_B >> Deg_C;
+
     double radC = (Deg_C/180.0)*M_PI;
+    int len_C = sqrt(pow(len_A, 2)+pow(len_B, 2)-2*len_A*len_B*cos(radC));
+    int height = len_B*sin(radC);
+
+    int area = height*len_A/2;
+    int circum = len_A + len_B + len_C;
+
+    cout << area << endl << circum << endl << height << endl;
     return 0;
 }
